9316be4a6be6729226b1deea2722315b0c0ceaa8
Seiko Takamori <a.n.t715seiko@gmail.com>
2021-03-22 09:31:29 +0900

    headerマークアップ

--- index.php
+++ index.php
@@ -4,7 +4,19 @@
 <meta charset="utf-8">
 <title>無題ドキュメント</title>
 </head>
-aaa
+
 <body>
+
+<header class="header">
+	<p class="header-logo"><img src="img/logo.png" srcset="img/logo.png 1,img/logo@2x.png 2x" alt=""></p>
+	<p class="header-txt">マーケティングのプロを育てる</p>
+	<nav class="gnav">
+		<span></span>
+		<span></span>
+	</nav>
+</header>	
+	
+	
+	
 </body>
 </html>
