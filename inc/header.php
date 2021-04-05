378d5c1a3dd962129a890efa6e12689f56cb9128
Seiko Takamori <a.n.t715seiko@gmail.com>
2021-04-05 09:31:56 +0900

    レスポンシブ　ハンバーガーメニュー設置

--- inc/header.php
+++ inc/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 <link rel="stylesheet" href="asset/css/style.css">
 <title>無題ドキュメント</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
